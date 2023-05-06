154f9c0843a93be834e66c6df368370fef6736d3
Nicholas Amancio <nick.amancio44@gmail.com>
2023-05-05 22:41:36 -0700

    added new files to grab photos from the Dog API

---
+++ src/Components/DogPictures.jsx
@@ -0,0 +1,66 @@
+/* import React from "react";
+
+const DogPictures = () => {
+    const [pictures, setPictures] = React.useState([]);
+    const [isDataLoaded, setIsDataLoaded] = React.useState(false);
+  
+    React.useEffect(() => {
+        fetch("https://dog.ceo/api/breed/bulldog/french/images")
+            .then((res) => res.json())
+            .then((json) => {
+                setPictures(json.message);
+                setIsDataLoaded(true);
+            });
+    }, []);
+
+    if (!isDataLoaded) {
+        return (
+            <p>Dogs are still loading...</p>
+        );
+    }
+  
+    return (
+        <div className="facts">
+            <p>These are Dogs</p>
+            {pictures.map((item) => (
+                <img src={item} alt={`Dog picture`} width="300px"/>
+            ))};
+        </div>
+    );
+};
+
+export default DogPictures; */
+
+import React from "react";
+
+const DogPictures = () => {
+    const [pictures, setPictures] = React.useState([]);
+    const [isDataLoaded, setIsDataLoaded] = React.useState(false);
+  
+    React.useEffect(() => {
+        fetch("https://dog.ceo/api/bulldog/french/images")
+            .then((res) => res.json())
+            .then((json) => {
+                setPictures(json.images);
+                setIsDataLoaded(true);
+            });
+    }, []);
+
+    if (!isDataLoaded) {
+        return (
+            <p>Dogs are still loading...</p>
+        );
+    }
+  
+    return (
+        <div className="facts">
+            <p>These are Dogs</p>
+            {pictures.map((item) => (
+                <img src={item.url} alt={`Dog picture`} width="300px"/>
+            ))}
+        </div>
+    );
+};
+
+export default DogPictures;
+
